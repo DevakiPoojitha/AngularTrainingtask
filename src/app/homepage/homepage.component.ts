9addf6f549c1d6ddb83d920c98cb8899203a5a3c
Devaki Poojitha <68280793+DevakiPoojitha@users.noreply.github.com>
2022-12-04 20:23:06 +0530

    Add files via upload

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+
+
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  
+
+  
+
+  ngOnInit(): void {
+    // this.data = this.dataService.getData();
+    // console.log(this.data);
+  }
+
+  onSubmit(){
+    
+  }
+
+}
